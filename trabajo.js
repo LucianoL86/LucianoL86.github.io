f5eeabb1c6572fcf8864938076b3c925040f4458
LucianoL86 <116621321+LucianoL86@users.noreply.github.com>
2022-12-14 17:42:13 -0300

    Add files via upload

---
+++ trabajo.js
@@ -0,0 +1,17 @@
+
+    var edad
+    edad = parseInt(prompt("Introduce tu edad"));
+    
+    if (edad >= 18) {
+        alert("Puedes entrar, eres mayor de edad")
+    }else {
+        alert("Eres menor de edad, entras bajo tu responsabilidad")
+    }
+    
+
+function linkExterno() {
+    alert("Usted está saliendo de nuestro sitio");
+}
+
+
+
